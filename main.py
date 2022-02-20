4b5fc70bcdbe8de0d02b2ed1482b15b89b3fc2ac
Alberto Gomez Juan <agomezjuan@hotmail.com>
2022-02-20 13:53:30 +0100

    Modificadas las rutas
    Se cambió el archivo main.py para que las rutas vayan por separado según el tipo de producto

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-from flask import Flask, render_template, url_for, redirect, request
-import db
-from models import Disco
+from flask import Flask, render_template
+from routes.disco import discos
+from routes.libro import libros
 
 app = Flask(__name__)
 
+app.register_blueprint(discos)
+app.register_blueprint(libros)
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    #     todos_los_discos = db.session.query(Disco).all()
-    #     for disco in todos_los_discos:
-    #         print(disco)
     return render_template('index.html')
-
-
-@app.route("/crear_disco", methods=["POST"])
-def crear():
-    disco = Disco(Nombre=request.form['nombre'],
-                  Precio=request.form['precio'])
-    db.session.add(disco)
-    db.session.commit()
-    db.session.close()
-    return redirect(url_for(home))
-
-
-if __name__ == ("__main__"):
-    db.Base.metadata.create_all(db.engine)
-    app.run(debug=True)

a916027e0f4e05b9003d2c3e5a9da817ba951ae7
Ray <ray@MacBook-Pro-de-Ramon.local>
2022-02-21 14:09:59 +0100

    borrar rutas del mein

--- main.py
+++ main.py
@@ -5,23 +5,6 @@ from models import Disco
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    #     todos_los_discos = db.session.query(Disco).all()
-    #     for disco in todos_los_discos:
-    #         print(disco)
-    return render_template('index.html')
-
-
-@app.route("/crear_disco", methods=["POST"])
-def crear():
-    disco = Disco(Nombre=request.form['nombre'],
-                  Precio=request.form['precio'])
-    db.session.add(disco)
-    db.session.commit()
-    db.session.close()
-    return redirect(url_for(home))
-
 
 if __name__ == ("__main__"):
     db.Base.metadata.create_all(db.engine)

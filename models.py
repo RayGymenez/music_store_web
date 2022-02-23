986bfbc7cff2193be3418a80a6b49c60902d4329
Alberto Gomez Juan <agomezjuan@hotmail.com>
2022-02-23 17:25:49 +0100

    ficheros actualizados

--- models.py
+++ models.py
@@ -20,3 +20,24 @@ class Disco(Base):
 
     def __repr__(self):
         return f"Disco {self.id}: {self.title} by {self.artist}"
+
+
+class Libro(Base):
+    __tablename__ = "libros"
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, nullable=False)
+    author = Column(Text, nullable=False)
+    editorial = Column(Text, nullable=False)
+    year = Column(Integer, nullable=False)
+    price = Column(Numeric, nullable=False)
+    cover = Column(Text)
+
+    def __init__(self, title, author, year, price, cover=None):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.price = price
+        self.cover = cover
+
+    def __repr__(self):
+        return f"Libro {self.id}: {self.title} escrito por {self.author} en {self.year}"
